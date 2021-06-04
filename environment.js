309f6a25f67481e6cacc51bd82897164ad4777f6
Daniel Berezin <daniel@berezin.io>
2021-06-03 18:01:32 -0700

    add setBuiltinClass to Environment
    Added an additional `setBuiltinClass` method to environment to make it easier to add builtin LoxClasses.

--- environment.js
+++ environment.js
@@ -26,6 +26,10 @@ class Environment {
     this.map.set(name, typeof func === 'function' ? { call: func } : func)
   }
 
+  setBuiltinClass(name, klass) {
+    this.map.set(name, JSClassToLoxClass(klass))
+  }
+
   assign(token, value) {
     if (!this.map.has(token.lexeme)) {
       if (this.enclosing) return this.enclosing.assign(token, value)
@@ -36,3 +40,24 @@ class Environment {
 }
 
 module.exports = Environment
+
+// We do all this below the export to avoid a circular dependancy
+const { LoxClass } = require('./interpreter')
+class FakeInstance {
+  constructor(call) {
+    this.call = call
+  }
+  bind(instance) {
+    this.call = this.call.bind(instance)
+    return this
+  }
+}
+
+const JSClassToLoxClass = klass => {
+  const methodNames = Object.getOwnPropertyNames(klass.prototype)
+  const methods = new Map(methodNames.map(name => [name, new FakeInstance(klass.prototype[name])]))
+  // TODO: Throw an error if user passes in a constructor
+  if (methods.has('constructor')) methods.delete('constructor')
+  const newKlass = new LoxClass(klass.name, methods)
+  return newKlass
+}
